cb1f9f73004d946fd58d5fbfdf06cca135d30576
Aaron Sergeev <a.sergeev@campus.tu-berlin.de>
2026-06-26 13:16:19 +0200

    Initial Commit

---
+++ SketchApp/SketchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchAppApp.swift
+//  SketchApp
+//
+//  Created by Aaron on 26.06.26.
+//
+
+import SwiftUI
+
+@main
+struct SketchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
